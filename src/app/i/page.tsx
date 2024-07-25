f58b7c386d32a8a05c9a9876ad5e1052cc38cb5c
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-07-25 21:53:32 +0300

    added middleware, statisitcs

--- src/app/i/page.tsx
+++ src/app/i/page.tsx
@@ -1,23 +1,23 @@
 import type { Metadata } from 'next';
 import React from 'react';
 
-// import { Heading } from '@/components/ui/Heading';
+import { Heading } from '@/components/ui/Heading';
 
-// import { NO_INDEX_PAGE } from '@/constants/seo.constants';
+import { NO_INDEX_PAGE } from '@/constants/seo.constants';
 
-// import { Statistics } from './Statistics';
+import { Statistics } from '@/app/i/Statistics';
 
 export const metadata: Metadata = {
 	title: 'Dashboard',
-	// ...NO_INDEX_PAGE,
+	...NO_INDEX_PAGE,
 };
 
 export default function DashboardPage() {
 	return (
 		<div>
 			Dashboard
-			{/* <Heading title='Statistics' />
-			<Statistics /> */}
+			<Heading title='Statistics' />
+			<Statistics />
 		</div>
 	);
 }
